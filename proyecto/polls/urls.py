b1f9db7ab5dbdd6873dbd7798c40cde7c2982197
saropi99 <saropi9978@gmail.com>
2021-03-10 12:54:59 -0500

    se agrega logo de grupo a login

--- proyecto/polls/urls.py
+++ proyecto/polls/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    path('login/',views.loggin,name='login'),
+    path('login/',views.login,name='login'),
 ]
--- proyecto/polls/views.py
+++ proyecto/polls/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def loggin(request):
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def login(request):
     return render(request,'login.html',context={'msg':'soy el login'})
