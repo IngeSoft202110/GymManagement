1c1fd8691183fbe319501f739cc25b6b73927280
saropi99 <saropi9978@gmail.com>
2021-05-18 21:40:50 -0500

    final

--- proyecto/polls/views.py
+++ proyecto/polls/views.py
@@ -265,26 +265,35 @@ def irSala(request):
     return render(request, 'sala.html', {'sala': request.POST['sala'], 'usuario': usuario, 'messages': messages})
 
 def listaUsuarioView(request):
-    usuario = request.POST.get('buscar')
+    if not checkPostRequest(request):
+        return render(request, 'login.html')
+    usuario = checkUser(request.POST['usr'])
+    print(request.POST['usr'])
     busqueda = request.POST.get('userBuscar')
     #ListaUsuario = checkUser(request.POST['buscar'])
     usuarios = Usuario.objects.all()
     #valor = Usuario.objects.filter(ListaUsuario = Usuario.nombre('user'), Usuario = usuarios.set())
     usuarioEncontrados = set()
     for ListaUsuario in usuarios:
-         if ListaUsuario.nombre.find(busqueda) >= 0:
+         if ListaUsuario.usuario.find(busqueda) >= 0:
              usuarioEncontrados.add(ListaUsuario)
     return render(request, 'listaUsuarioView.html', {'usuario' : usuario, 'usuarios' : usuarioEncontrados})
 
 def verPerfilUsuarioView(request):
+
+    mismo = request.POST['user1']== request.POST.get('usuario')
+    print(request.POST['user1'])
+    print(request.POST.get('usuario'))
     usuario= checkUser(request.POST['user1'])
-    usuarios = request.POST.get('usuario1')
+    usuarios = request.POST.get('usuario')
+
     rutinas = Rutina.objects.all()
     rutinasCreadas = set()
+
     for rutina in rutinas:
         if rutina.usuario == usuario:
             rutinasCreadas.add(rutina)
-    return render(request, 'verPerfilUsuario.html', {'usuario': usuario, 'rutinas' : rutinasCreadas})
+    return render(request, 'verPerfilUsuario.html', {'usuario': usuario, 'rutinas' : rutinasCreadas, 'mismo':mismo})
 
 def seguirRutina(request):
     print(request.POST)
